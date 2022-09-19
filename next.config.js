f54fdbfde1dd278f6392316ebb84651980056c8a
jona872 <jona_872@hotmail.com>
2022-09-19 10:38:20 -0300

    mapbox

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_TEST=hello,
+    NEXT_PUBLIC_MAPBOX_TOKEN=sk.eyJ1Ijoiam9uYXRhbmtpbiIsImEiOiJjbDg3bzA3MnkxYjdxM29waG4wMmZnMGJpIn0.tDwLJXdcQDhSh4oPgcqPnA,
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (<>
+    <Head>
+      <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+    </Head>
     <Component {...pageProps} />
     <ToastContainer />
   </>)
